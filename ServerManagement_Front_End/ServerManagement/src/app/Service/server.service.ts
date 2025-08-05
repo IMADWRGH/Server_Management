7dd36aefbdc7852d1e45729317fcf67d2c9c597a
Imad Asserne <imad.asserne@gmail.com>
2025-08-05 04:43:44 +0100

    fix: update TypeScript strict mode to false and refactor server service imports

--- ServerManagement_Front_End/ServerManagement/src/app/Service/server.service.ts
+++ ServerManagement_Front_End/ServerManagement/src/app/Service/server.service.ts
@@ -1,78 +1,78 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {catchError, tap} from "rxjs/operators";
-import {Observable, throwError} from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError, tap } from "rxjs/operators";
+import { Observable, throwError } from "rxjs";
 
-import {CustomResponse} from "../interface/custom-response";
-import {Server} from "../interface/server";
-import {Status} from "../enum/status.enum";
+import { CustomResponse } from "../interface/custom-response";
+import { Server } from "../interface/server";
+import { Status } from "../enum/status.enum";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
-  private readonly apiUrl=`http://localhost:8080/server`;
-  constructor(private http:HttpClient) { }
-   servers$=<Observable<CustomResponse>>
-     this.http.get<CustomResponse>(`${this.apiUrl}/list`)
-       .pipe(
-       tap(console.log),
-        catchError(this.handleError)
-       );
-
-  save$=(server:Server)=> <Observable<CustomResponse>>
-    this.http.post<CustomResponse>(`${this.apiUrl}/list`,server)
+  private readonly apiUrl = `http://localhost:8080/server`;
+  constructor(private http: HttpClient) { }
+  servers$ = <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/list`)
       .pipe(
         tap(console.log),
         catchError(this.handleError)
       );
 
-  ping$=(ipAddress:String)=> <Observable<CustomResponse>>
+  save$ = (server: Server) => <Observable<CustomResponse>>
+    this.http.post<CustomResponse>(`${this.apiUrl}/save`, server)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  ping$ = (ipAddress: String) => <Observable<CustomResponse>>
     this.http.get<CustomResponse>(`${this.apiUrl}/ping/${ipAddress}`)
       .pipe(
         tap(console.log),
         catchError(this.handleError)
       );
+  /// this method is use "strict": true
+  // filter$ = (status: Status, response: CustomResponse) =>
+  //   new Observable<CustomResponse>(subscriber => {
+  //     console.log(response);
 
-  filter$ = (status: Status, response: CustomResponse) =>
-    new Observable<CustomResponse>(subscriber => {
-      console.log(response);
+  //     if (response.data?.servers) {
+  //       const filteredServers = response.data.servers.filter(
+  //         server => server.status === status
+  //       );
+  //       subscriber.next({
+  //         ...response,
+  //         message: status === Status.All
+  //           ? `Servers filtered by ${status} status`
+  //           : filteredServers.length > 0
+  //             ? `Servers filtered by ${
+  //               status === Status.SERVER_UP ? 'SERVER UP' : 'SERVER DOWN'
+  //             } status`
+  //             : `No servers of ${status} found`,
+  //         data: {
+  //           servers: filteredServers
+  //         }
+  //       });
+  //     } else {
+  //       subscriber.next({
+  //         ...response,
+  //         message: "No servers found",
+  //         data: {
+  //           servers: [],
+  //         },
+  //       });
+  //     }
 
-      if (response.data?.servers) {
-        const filteredServers = response.data.servers.filter(
-          server => server.status === status
-        );
-        subscriber.next({
-          ...response,
-          message: status === Status.All
-            ? `Servers filtered by ${status} status`
-            : filteredServers.length > 0
-              ? `Servers filtered by ${
-                status === Status.SERVER_UP ? 'SERVER UP' : 'SERVER DOWN'
-              } status`
-              : `No servers of ${status} found`,
-          data: {
-            servers: filteredServers
-          }
-        });
-      } else {
-        subscriber.next({
-          ...response,
-          message: "No servers found",
-          data: {
-            servers: [],
-          },
-        });
-      }
+  //     subscriber.complete();
+  //   })
+  //     .pipe(
+  //       tap(console.log),
+  //       catchError(this.handleError)
+  //     );
 
-      subscriber.complete();
-    })
-      .pipe(
-        tap(console.log),
-        catchError(this.handleError)
-      );
-
-  delete$=(serverId:number)=> <Observable<CustomResponse>>
+  delete$ = (serverId: number) => <Observable<CustomResponse>>
     this.http.delete<CustomResponse>(`${this.apiUrl}/delete/${serverId}`)
       .pipe(
         tap(console.log),
@@ -80,38 +80,40 @@ export class ServerService {
       );
 
 
-  private handleError(error:HttpErrorResponse): Observable<never> {
+  private handleError(error: HttpErrorResponse): Observable<never> {
     console.log(error);
     return throwError(() => new Error(`Error :${error.status}`));
   }
 
 
 
-  ///// filter on syntax diff
-  // filter$ = (status:Status, response:CustomResponse)=> <Observable<CustomResponse>>
-  //   new Observable<CustomResponse>(
-  //     subscriber => {
-  //       console.log(response);
-  //
-  //       subscriber.next(
-  //         status === Status.All ? {...response, message: `Servers filtered by ${status} status`} :
-  //           {
-  //             ...response,
-  //             message: response.data.servers?.filter(server => server.status === status).length > 0 ?
-  //               `Servers filter by${status === Status.SERVER_UP ?
-  //                 `SERVER UP` : `SERVER DOWN`} status`
-  //               : `No servers of ${status} found`,
-  //             data: {
-  //               servers: response.data.servers?.filter(server => server.status === status)
-  //             }
-  //           }
-  //       );
-  //       subscriber.complete();
-  //     }
-  //   ).pipe(
-  //     tap(console.log),
-  //     catchError(this.handleError)
-  //   );
+  /// filter on syntax diff
+  /// I'm change value of "strict": false
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+    new Observable<CustomResponse>(
+      subscriber => {
+        console.log(response);
+
+        subscriber.next(
+          status === Status.All ? { ...response, message: `Servers filtered by ${status} status` } :
+            {
+              ...response,
+              message: response.data.servers
+                .filter(server => server.status === status).length > 0 ?
+                `Servers filter by${status === Status.SERVER_UP ?
+                  `SERVER UP` : `SERVER DOWN`} status`
+                : `No servers of ${status} found`,
+              data: {
+                servers: response.data.servers.filter(server => server.status === status)
+              }
+            }
+        );
+        subscriber.complete();
+      }
+    ).pipe(
+      tap(console.log),
+      catchError(this.handleError)
+    );
 
 
 }
